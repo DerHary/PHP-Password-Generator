9ba8042e3da0ce69e671e679c94d65416b869239
Hary <hary74656@weavernet.at>
2025-07-20 14:14:10 +0200

    File move

--- PasswordGenerator.php
+++ PasswordGenerator.php
@@ -144,7 +144,7 @@ for ($j = 0; $j < $count; $j++) {
 <head>
     <meta charset="UTF-8">
     <title><?= $t['title'] ?></title>
-    <link rel="icon" type="image/png" href="favicon.ico"/>
+    <link rel="icon" type="image/png" href="img/favicon.ico"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .card-copied {
